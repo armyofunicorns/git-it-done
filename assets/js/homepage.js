e44478c5f7148265d7dc84b5fe4472247f1793df
Anthony Hall <anthony.w.hall@gmail.com>
2023-03-17 23:58:52 -0700

    Added a form to the page.

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,10 +1,46 @@
 /* Created by Anthony Hall
 Updated on March 17, 2023 */
 
-// Get All the Repos By Users
-var getUserRepos = function() {
-    console.log("function was called");
-    fetch("https://api.github.com/users/armyofunicorns/repos");
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+    console.log(event);
 };
-  
-getUserRepos();
+
+// Get All the Repos By Users
+var getUserRepos = function(user) {
+    console.log("start");
+
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+    
+    // var response = fetch("https://api.github.com/users/armyofunicorns/repos");
+    // console.log(response);
+    // fetch("https://api.github.com/users/armyofunicorns/repos").then(function(response) {
+    //     response.json().then(function(data) {
+    //         console.log(data);
+    //     });    
+    // });
+     
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+        console.log(data);
+        });
+    });
+    console.log("outside");
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+// getUserRepos(username);
